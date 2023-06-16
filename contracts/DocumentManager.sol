1981a6e917326456fe98eec0ca9df94be09a1737
vivekup3424 <vivek.hygfrt324@gmail.com>
2023-06-16 18:22:55 +0530

    added deploy scripts

---
+++ contracts/DocumentManager.sol
@@ -0,0 +1,107 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+//imports
+import "./UserManager.sol";
+
+contract DocumentManager {
+    //modifiers
+    modifier onlyRegisteredUser() {
+        require(
+            userManager.getUser(msg.sender).accountAddress == msg.sender,
+            "User is not registered"
+        );
+        _;
+    }
+
+    //structure to represent a Document
+    struct Document {
+        uint256 documentId;
+        string documentName;
+        address ownerAddress;
+        string ipfsHash;
+    }
+
+    //mapping to store documents by user
+    mapping(address => Document[]) private userToDocuments;
+
+    UserManager private userManager;
+    uint256 private documentIdCounter; //unique to each document
+    Document[] private documents;
+
+    //constructor
+    constructor(UserManager _userManager) {
+        userManager = _userManager;
+        documentIdCounter = 0;
+    }
+
+    /**
+     * @dev Uploads a new document for a user.
+     * @param documentName The name of the document.
+     * @param ipfsHash The IPFS hash of the document.
+     * @param userAddress The address of the user.
+     */
+    function uploadDocument(
+        string memory documentName,
+        string memory ipfsHash,
+        address userAddress
+    ) external onlyRegisteredUser {
+        require(bytes(ipfsHash).length > 0, "IPFS Hash cannot be empty.");
+        Document memory newDocument = Document({
+            documentId: documentIdCounter,
+            documentName: documentName,
+            ipfsHash: ipfsHash,
+            ownerAddress: userAddress
+        });
+        //add the new document to the mapping
+        userToDocuments[userAddress].push(newDocument);
+        documents.push(newDocument);
+        documentIdCounter++;
+    }
+
+    /**
+     * @dev Retrieves all documents for a user.
+     * @param userAddress The address of the user.
+     * @return An array of Document structs representing the user's documents.
+     */
+    function getUserDocuments(
+        address userAddress
+    ) external view onlyRegisteredUser returns (Document[] memory) {
+        return userToDocuments[userAddress];
+    }
+
+    /**
+     * @dev Retrieves a document by its ID.
+     * @param documentId The ID of the document.
+     * @return The Document struct representing the document.
+     */
+    function getDocument(
+        uint256 documentId
+    ) public view onlyRegisteredUser returns (Document memory) {
+        require(documentId < documents.length, "Invalid document ID");
+        return documents[documentId];
+    }
+
+    /**
+     * @dev Checks if a document exists.
+     * @param documentId The ID of the document.
+     * @return A boolean indicating whether the document exists or not.
+     */
+    function documentExists(
+        uint256 documentId
+    ) public view onlyRegisteredUser returns (bool) {
+        return documentId < documents.length;
+    }
+
+    /**
+     * @dev Retrieves the owner address of a document.
+     * @param documentId The ID of the document.
+     * @return The address of the document owner.
+     */
+    function getDocumentOwner(
+        uint256 documentId
+    ) public view onlyRegisteredUser returns (address) {
+        require(documentExists(documentId), "Document does not exist");
+        return documents[documentId].ownerAddress;
+    }
+}
--- contracts/UserManager.sol
+++ contracts/UserManager.sol
@@ -5,16 +5,20 @@ pragma solidity ^0.8.8;
 error UserManager__InvalidDOB(bytes32 rollNumber, bytes32 DOB);
 error UserManager__InvalidPrivateKey(address accountAddress);
 error UserManager__InvalidPassword(string emailId, string password);
+error UserManager__RollNumberNotFound(bytes32 rollNumber);
 
 /**
  * @title UserManager
- * @author Vivek Upadhyay
  * @notice A contract to register and login users
  */
 contract UserManager {
     //events
     event UserRegistered(bytes32 indexed rollNumber, bytes32 DOB);
     event UserLoggedIn(address accountAddress);
+    event AdminRegistered(string indexed emailId, address adminAddress);
+    event AdminLoggedIn(address accountAddress);
+
+    //structs for User and Admin
     struct User {
         bytes32 rollNumber;
         bytes32 dateOfBirth;
@@ -27,70 +31,122 @@ contract UserManager {
 
     //variables
     string private constant PASSWORD = "something_else";
-    mapping(bytes32 => bytes32) private rollNumberToDOB;
-    mapping(address => User) private users;
-    Admin[] private admins;
+    mapping(bytes32 => bytes32) private rollNumberToDOB; //sign-up
+    mapping(address => User) private users; //for login
+    // Declare an array to store user addresses
+    address[] private userAddresses; //for finding address from roll_number
+    mapping(address => Admin) private admins; //login of Admins (O(logn) time complexity)
 
-    //storing the DateOfBirths
-    //IMP -  consider the huge amount of data we are storing in the mapping won't
-    //it increase the cost of deployment of the contract
-    //how to optimize it
-
-    //Regsiter a new user
+    /**
+     * @notice Registers a new user
+     * @param DOB The date of birth of the user
+     * @param rollNumber The roll number of the user
+     */
     function registerUser(bytes32 DOB, bytes32 rollNumber) external {
-        if (
-            rollNumberToDOB[rollNumber] != DOB
-        ) //we already have data for all students
-        {
+        if (rollNumberToDOB[rollNumber] != DOB) {
             revert UserManager__InvalidDOB(rollNumber, DOB);
         }
-        //we need to connect to a wallet in order to have a users address
-        //since we can't by ourselves generate a address, as we also need to manage it on a blockchain
         User memory newUser = User({
             rollNumber: rollNumber,
             dateOfBirth: DOB,
-            accountAddress: msg.sender //this is good as register button is used by the user hence sender would be the User only
+            accountAddress: msg.sender
         });
         users[msg.sender] = newUser;
+        userAddresses.push(msg.sender);
         emit UserRegistered(rollNumber, DOB);
     }
 
-    //Login a User by verifying their private key
+    /**
+     * @notice Logs in a user by verifying their private key
+     * @return result True if the login is successful, false otherwise
+     */
     function loginUser() external returns (bool result) {
         if (users[msg.sender].rollNumber == "") {
-            return false; //I think msg.sender is good here
+            return false;
         }
+        emit UserLoggedIn(msg.sender);
         return true;
     }
 
-    //get a particular user details
-    function getUser(address UserAddress) external view returns (User memory) {
-        if (users[UserAddress].rollNumber == "") {
-            revert UserManager__InvalidPrivateKey(UserAddress); //I need to make this statement more intuitive
+    /**
+     * @notice Gets the details of a particular user
+     * @param userAddress The address of the user
+     * @return user The user details
+     */
+    function getUser(address userAddress) external view returns (User memory) {
+        if (users[userAddress].rollNumber == "") {
+            revert UserManager__InvalidPrivateKey(userAddress);
         }
-        return users[UserAddress];
+        return users[userAddress];
     }
 
-    //Register a new Admin
+    //Admin Functionalities
+    /**
+     * @notice Registers a new admin
+     * @param emailId The email ID of the admin
+     * @param password The password of the admin
+     */
     function registerAdmin(
         string memory emailId,
         string memory password
     ) public {
         if (keccak256(bytes(password)) != keccak256(bytes(PASSWORD))) {
-            //comparing the hash values of two functions
             revert UserManager__InvalidPassword(emailId, password);
         }
         Admin memory newAdmin = Admin({emailId: emailId, account: msg.sender});
-        admins.push(newAdmin);
+        admins[msg.sender] = newAdmin;
+        emit AdminRegistered(emailId, msg.sender);
     }
 
-    // Login Admin
+    /**
+     * @notice Logs in an admin
+     * @return result True if the login is successful, false otherwise
+     */
     function loginAdmin() external returns (bool result) {
-        for (uint256 i = 0; i < admins.length; i++) {
-            if (admins[i].account == msg.sender) {
-                return true; // Admin found
+        if (admins[msg.sender].account == address(0)) {
+            return false;
+        }
+        emit AdminLoggedIn(msg.sender);
+        return true;
+    }
+
+    /**
+     * @notice Sets the date of birth for a given roll number
+     * @param rollNumber The roll number of the user
+     * @param dateOfBirth The date of birth of the user
+     */
+    function setRollNumberToDOB(
+        bytes32 rollNumber,
+        bytes32 dateOfBirth
+    ) external {
+        rollNumberToDOB[rollNumber] = dateOfBirth;
+    }
+
+    /**
+     * @notice Gets the date of birth for a given roll number
+     * @param rollNumber The roll number of the user
+     * @return dateOfBirth The date of birth of the user
+     */
+    function getRollNumberToDOB(
+        bytes32 rollNumber
+    ) external view returns (bytes32) {
+        return rollNumberToDOB[rollNumber];
+    }
+
+    /**
+     * @notice Finds the account address by roll number
+     * @param rollNumber The roll number of the user
+     * @return accountAddress The account address of the user
+     */
+    function findAccountAddressByRollNumber(
+        bytes32 rollNumber
+    ) public view returns (address) {
+        for (uint256 i = 0; i < userAddresses.length; i++) {
+            address userAddress = userAddresses[i];
+            if (users[userAddress].rollNumber == rollNumber) {
+                return userAddress;
             }
         }
-        return false; // Admin not found
+        revert UserManager__RollNumberNotFound(rollNumber);
     }
 }
